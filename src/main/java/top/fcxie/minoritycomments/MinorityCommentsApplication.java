7170cd260ba0e2dbf829ee791fc0002865804ef6
富畴 谢 <fuchouxie@163.com>
2022-09-02 16:12:02 +0800

    build:构建工程

---
+++ src/main/java/top/fcxie/minoritycomments/MinorityCommentsApplication.java
@@ -0,0 +1,13 @@
+package top.fcxie.minoritycomments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinorityCommentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinorityCommentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/fcxie/minoritycomments/MinorityCommentsApplicationTests.java
@@ -0,0 +1,13 @@
+package top.fcxie.minoritycomments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinorityCommentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
